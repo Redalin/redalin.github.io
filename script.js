c180b9128e5d5f82168e30924a05a10cd1c0cbc4
criddell-eb <39214477+Redalin@users.noreply.github.com>
2025-03-16 13:08:02 +1300

    replace with working files and buttons
    Merged in from DRWCallerESP code

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ function updateTeamName(selectElement, teamId) {
     document.querySelector(`#${teamId} .team-name`).textContent = selectedTeamName;
 }
 
-function pilotSwap(teamId) {
+function pilotSwap(teamId, buttonId) {
     const teamBox = document.getElementById(teamId);
     const teamName = teamBox.querySelector('.team-name').textContent;
+    const button = document.getElementById(buttonId);
+
     teamBox.style.backgroundColor = 'yellow';
-    setTimeout(() => {
-        teamBox.style.backgroundColor = '';
-    }, 10000);
 
     const announcement = new SpeechSynthesisUtterance(`Pilot swap announced: ${teamName}`);
     window.speechSynthesis.speak(announcement);
+
+    button.disabled = true;
+
+    setTimeout(() => {
+        teamBox.style.backgroundColor = '';
+        button.disabled = false;
+    }, 20000);
+
+
 }
